444a16185f645bafe6466b0878b9144be0269371
Pavlenty <brosok05@bk.ru>
2025-07-11 12:12:17 +0300

    Настроил логирование файлов (папка Logs, файлик app.log). Сделал возможность менять путь к логам. Добавил парочку новых логов.

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class MainWindow(QtWidgets.QMainWindow):
         # Инициализация видеозахвата
         self.cap = cv2.VideoCapture(0)  # 0 - камера по умолчанию
         if not self.cap.isOpened():
-            self.ui.video_frame.setText("Не удалось открыть камеру")
-            print("Не удалось открыть камеру")
+            logger.error("Не удалось открыть камеру")
+            QtWidgets.QMessageBox.critical(self, "Ошибка", "Не удалось открыть камеру.  Проверьте подключение.")
             sys.exit()
 
         # Настройка таймера
@@ -24,16 +24,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(30)  # Обновление каждые 30 мс
 
+        self.path_logs = "D:/Python projects/Defect_detection_app/Logs"
+        self.ui.log_path_edit.setText(self.path_logs)
+
+        logger.add(sys.stderr, format="{time} {level} {message}\n", level="INFO")
+        logger.add(os.path.join(self.path_logs, "app.log"), rotation="500 MB", level="DEBUG", encoding="utf8", format="{time} {level} {message}\n")
+
         self.setup_connections()
 
     def setup_connections(self):
         """Подключение сигналов (поступающих событий) к слотам (обработчикам событий)"""
-        self.ui.log_path_button.clicked.connect(self.choose_log_path)
         self.ui.sensivity_slider.valueChanged.connect(self.update_sensitivity_label)
         self.ui.save_button.clicked.connect(self.save_frame)
+        self.ui.log_path_button.clicked.connect(self.choose_logs_path)
 
     def update_frame(self):
-        """Обработка поступающих кадров и помещении их в QLabel (video_frame)"""
+        """Обработка поступающих кадров и помещение их в QLabel (video_frame)"""
         ret, frame = self.cap.read()  # ret - булевская переменная (true, если кадр был успешно считан), frame - кадр видео в виде массива NumPy.
         if ret:
             # Преобразование кадра OpenCV в формат QImage
@@ -47,9 +53,23 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.video_frame.setPixmap(pixmap)
 
     def closeEvent(self, event):
-        """Обработка закрытия окна и освобождения ресурсов веб-камеры"""
-        self.cap.release()
-        event.accept()
+        """Обработка закрытия окна приложения и освобождения ресурсов веб-камеры"""
+        reply = QtWidgets.QMessageBox.question(
+            self, 'Закрыть окно',
+            'Вы уверены, что хотите закрыть окно?',
+            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+            QtWidgets.QMessageBox.No
+        )
+
+        if reply == QtWidgets.QMessageBox.Yes:
+            try:
+                self.cap.release()
+                logger.info("Ресурсы камеры успешно освобождены")
+            except Exception as e:
+                logger.exception(f"Ошибка при освобождении ресурсов камеры: {e}")
+            event.accept()
+        else:
+            event.ignore()
 
     def save_frame(self):
         """Сохранение кадра при нажатии на элемент окна - save_button"""
@@ -63,8 +83,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
             try:
                 cv2.imwrite(filepath, frame)  # Сохраняем в BGR
-
                 self.add_defective_board_info(filename, filepath)
+                logger.info(f"Сохранен кадр бракованной платы: {filepath}")
             except Exception as e:
                 QtWidgets.QMessageBox.critical(self, "Ошибка", f"Ошибка при сохранении кадра: {e}")
                 logger.exception(f"Ошибка при сохранении кадра: {e}")  # логирование ошибок
@@ -76,9 +96,28 @@ class MainWindow(QtWidgets.QMainWindow):
         item = QtWidgets.QListWidgetItem(f"Имя: {filename}\nПуть: {filepath}")
         self.ui.defective_board_list.addItem(item)
 
-    def choose_log_path(self):
-        # Логика выбора пути
-        print("Выбор пути...")
+    def choose_logs_path(self):
+        """Выбор нового пути и замена старого."""
+        new_path_logs = QtWidgets.QFileDialog.getExistingDirectory(
+            self,
+            "Выберите папку для логов",
+            self.path_logs  # Начинаем с текущего пути (если он задан)
+        )
+
+        # Если пользователь выбрал путь (не нажал "Отмена")
+        if new_path_logs:
+            self.path_logs = new_path_logs
+            self.ui.log_path_edit.setText(self.path_logs)
+
+            self.configure_loguru(self.path_logs)
+
+            logger.info(f"Путь к логам изменен на: {self.path_logs}")
+
+    def configure_loguru(self, path_logs):
+        """Перенастройка loguru: перемещение файлов логирования в новую папку."""
+        logger.remove()
+        logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")  # Добавить вывод в консоль
+        logger.add(os.path.join(path_logs, "app.log"), rotation="500 MB", level="DEBUG", encoding="utf8")
 
     def update_sensitivity_label(self, value):
         """Отображение числового значения ползунка"""
